d758026c7f32607b9b78c564d3f90e38bd4343b1
Thiago Menezes <thimenezes19@gmail.com>
2023-09-29 21:11:34 -0300

    App Funcionando

--- App.js
+++ App.js
@@ -1,31 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [nota1, setNota1] = useState('');
+  const [nota2, setNota2] = useState('');
+  const [media, setMedia] = useState(null)
+
+  const handleNota1Change = (nota) => {
+    setNota1(nota);
+  };
+
+  const handleNota2Change = (nota) => {
+    setNota2(nota);
+  };
+
+  const handleSubmite = () => {
+    const n1 = parseFloat(nota1);
+    const n2 = parseFloat(nota2);
+
+    if (isNaN(n1) || isNaN(n2)) {
+      setMedia(null)
+    } else {
+      const mediaCalculada = (n1 + n2)/2
+      setMedia(mediaCalculada.toFixed(2))
+    }
+    Keyboard.dismiss();
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerBox}>
         <Text style={styles.containerText}>Média de Notas</Text>
+
         <TextInput
+        onChangeText={handleNota1Change}
+        value={nota1}
         placeholder='Nota 1'
         style={styles.input}
+        keyboardType='numeric'
         ></TextInput>
+
         <TextInput
+        onChangeText={handleNota2Change}
+        value={nota2}
         placeholder='Nota 2'
         style={styles.input}
+        keyboardType='numeric'
         ></TextInput>
-        <TouchableOpacity 
+
+        <TouchableOpacity
+        onPress={handleSubmite}
         style={styles.button}
         >
           <Text style={styles.buttonText}>Calcular</Text>
         </TouchableOpacity>
+
         <StatusBar style="auto" />
       </View>
+
       <View style={styles.containerResult}>
-        <Text 
-        style={styles.resultText}
-        >Média: 9</Text>
+        {media !== null && <Text style={styles.resultText}>Média: {media}</Text>}
       </View>
+
     </View>
   );
 }
@@ -54,8 +92,8 @@ const styles = StyleSheet.create({
   },
   input:{
     backgroundColor: '#e2fbfa',
-    paddingVertical: 6,
-    paddingHorizontal: 85,
+    height: 40,
+    width: 200,
     borderRadius: 10,
     elevation: 2, 
   },
